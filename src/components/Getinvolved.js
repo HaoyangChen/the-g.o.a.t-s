6659716dd162b6eacfc0f42ec4d057057a067d62
Rahma Kamel <kamelr@uw.edu>
2020-11-28 14:54:04 -0800

    finalized get involved page, waiting on text

--- src/components/Getinvolved.js
+++ src/components/Getinvolved.js
@@ -1,7 +1,81 @@
 import React from "react";
-import { Card, Button, Container, Row, Col, Accordion } from 'react-bootstrap';
+import { Card, Button, Container, Row, Col, Modal } from 'react-bootstrap';
+import TemplatedEmail from './TemplatedEmail';
 
 const Getinvolved = () => {
+  function HTEmail(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Human Trafficking
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <TemplatedEmail />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button style={{ backgroundColor: '#F5F5F5', borderColor: "#F5F5F5", color: "#4C4AC2" }} onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  function STEmail(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Sex Trafficking
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <TemplatedEmail />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button style={{ backgroundColor: '#F5F5F5', borderColor: "#F5F5F5", color: "#4C4AC2" }} onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  function FSEmail(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Forced Sterilization
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <TemplatedEmail />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button style={{ backgroundColor: '#F5F5F5', borderColor: "#F5F5F5", color: "#4C4AC2" }} onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+  const [HTmodalShow, HTsetModalShow] = React.useState(false);
+  const [STmodalShow, STsetModalShow] = React.useState(false);
+  const [FSmodalShow, FSsetModalShow] = React.useState(false);
+
+
   return (
     <main>
       <Card className="bg-light text-black">
@@ -9,18 +83,20 @@ const Getinvolved = () => {
         <Card.ImgOverlay>
           <Card.Title><h1 style={{ fontSize: 80 }}>Get Involved</h1></Card.Title>
           <Card.Text>
-            <p style={{ fontSize: 20 }}> This is the page for Get Involved</p>
+            <p style={{ fontSize: 20 }}> Take Action to Create Lasting Change</p>
           </Card.Text>
         </Card.ImgOverlay>
       </Card>
       <Container>
         <br></br>
+        <br></br>
         <Row>
           <Col lg={7}><p align='left'>Once you understand the complexities of sex and human trafficking, you can get involved and help be an ally and advocate for both victims and survivors in your communities. By signing petitions, raising funds, spreading awareness, and taking other steps, you can create meaningful change and fight against human and sex trafficking. </p>
             <p align='left'>Once you understand the complexities of sex and human trafficking, you can get involved and help be an ally and advocate for both victims and survivors in your communities. By signing petitions, raising funds, spreading awareness, and taking other steps.</p>
           </Col>
-          <Col lg={5}><img src='GetInvolvedHeader.svg' height='200'></img></Col>
+          <Col lg={5}><img src='headerImage.svg' height='200'></img></Col>
         </Row>
+        <br></br>
         <br></br>
       </Container>
       <Container>
@@ -29,10 +105,18 @@ const Getinvolved = () => {
             <Card>
               <Card.Header style={{ color: '#4C4AC2' }} align="left"><h4>Stay Informed</h4></Card.Header>
               <Card.Body>
-                <Card.Title align="left"><h5><em>Why staying educated can save lives</em></h5></Card.Title>
-                <Card.Text align="left">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consequat venenatis est non euismod. Mauris quis sem ex. Sed eros nulla, gravida ut diam et, lobortis ullamcorper nibh. Mauris consequat faucibus quam, id suscipit massa mollis in. Curabitur rutrum magna dui. Vivamus vitae finibus est, id tincidunt velit. Mauris pellentesque magna vel magna fringilla, nec tincidunt sapien blandit. Curabitur imperdiet, lacus at varius elementum, leo lorem interdum dui, tempus bibendum sem tellus eget ante.              </Card.Text>
-                {/* <Button variant="primary">Go somewhere</Button> */}
+                <Row>
+                  <Col sm={3}>
+                    <img src='inform.svg' height='100'></img>
+                  </Col>
+                  <Col sm={9}>
+                    <Card.Title align="left"><h5><em>Why staying educated can save lives</em></h5></Card.Title>
+                    <Card.Text align="left">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consequat venenatis est non euismod. Mauris quis sem ex. Sed eros nulla, gravida ut diam et, lobortis ullamcorper nibh. Mauris consequat faucibus quam, id suscipit massa mollis in. Curabitur rutrum magna dui. Vivamus vitae finibus est, id tincidunt velit. Mauris pellentesque magna vel magna fringilla, nec tincidunt sapien blandit. Curabitur imperdiet, lacus at varius elementum, leo lorem interdum dui, tempus bibendum sem tellus eget ante.              </Card.Text>
+                    {/* <Button variant="primary">Go somewhere</Button> */}
+                  </Col>
+                </Row>
+
               </Card.Body>
             </Card>
           </Col>
@@ -40,10 +124,17 @@ const Getinvolved = () => {
             <Card>
               <Card.Header style={{ color: '#4C4AC2' }} as="h5" align="left"><h4>Vote</h4></Card.Header>
               <Card.Body>
-                <Card.Title align="left"><h5><em>Exercise your right and vote now</em></h5></Card.Title>
-                <Card.Text align="left">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consequat venenatis est non euismod. Mauris quis sem ex. Sed eros nulla, gravida ut diam et, lobortis ullamcorper nibh. Mauris consequat faucibus quam, id suscipit massa mollis in. Curabitur rutrum magna dui. Vivamus vitae finibus est, id tincidunt velit. Mauris pellentesque magna vel magna fringilla, nec tincidunt sapien blandit. Curabitur imperdiet, lacus at varius elementum, leo lorem interdum dui, tempus bibendum sem tellus eget ante.              </Card.Text>
-                {/* <Button variant="primary">Go somewhere</Button> */}
+                <Row>
+                  <Col sm={3}>
+                    <img src='vote.svg' height='100'></img>
+                  </Col>
+                  <Col sm={9}>
+                    <Card.Title align="left"><h5><em>Exercise your right and vote now</em></h5></Card.Title>
+                    <Card.Text align="left">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consequat venenatis est non euismod. Mauris quis sem ex. Sed eros nulla, gravida ut diam et, lobortis ullamcorper nibh. Mauris consequat faucibus quam, id suscipit massa mollis in. Curabitur rutrum magna dui. Vivamus vitae finibus est, id tincidunt velit. Mauris pellentesque magna vel magna fringilla, nec tincidunt sapien blandit. Curabitur imperdiet, lacus at varius elementum, leo lorem interdum dui, tempus bibendum sem tellus eget ante.              </Card.Text>
+                    {/* <Button variant="primary">Go somewhere</Button> */}
+                  </Col>
+                </Row>
               </Card.Body>
             </Card>
           </Col>
@@ -54,10 +145,17 @@ const Getinvolved = () => {
             <Card>
               <Card.Header style={{ color: '#4C4AC2' }} as="h5" align="left"><h4>Volunteer</h4></Card.Header>
               <Card.Body>
-                <Card.Title align="left"><h5><em>Create a lasting impact</em></h5></Card.Title>
-                <Card.Text align="left">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consequat venenatis est non euismod. Mauris quis sem ex. Sed eros nulla, gravida ut diam et, lobortis ullamcorper nibh. Mauris consequat faucibus quam, id suscipit massa mollis in. Curabitur rutrum magna dui. Vivamus vitae finibus est, id tincidunt velit. Mauris pellentesque magna vel magna fringilla, nec tincidunt sapien blandit. Curabitur imperdiet, lacus at varius elementum, leo lorem interdum dui, tempus bibendum sem tellus eget ante.              </Card.Text>
-                {/* <Button variant="primary">Go somewhere</Button> */}
+                <Row>
+                  <Col sm={3}>
+                    <img src='volunteer.svg' height='90'></img>
+                  </Col>
+                  <Col sm={9}>
+                    <Card.Title align="left"><h5><em>Create a lasting impact</em></h5></Card.Title>
+                    <Card.Text align="left">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consequat venenatis est non euismod. Mauris quis sem ex. Sed eros nulla, gravida ut diam et, lobortis ullamcorper nibh. Mauris consequat faucibus quam, id suscipit massa mollis in. Curabitur rutrum magna dui. Vivamus vitae finibus est, id tincidunt velit. Mauris pellentesque magna vel magna fringilla, nec tincidunt sapien blandit. Curabitur imperdiet, lacus at varius elementum, leo lorem interdum dui, tempus bibendum sem tellus eget ante.              </Card.Text>
+                    {/* <Button variant="primary">Go somewhere</Button> */}
+                  </Col>
+                </Row>
               </Card.Body>
             </Card>
           </Col>
@@ -65,19 +163,26 @@ const Getinvolved = () => {
             <Card>
               <Card.Header style={{ color: '#4C4AC2' }} as="h5" align="left"><h4>Donate</h4></Card.Header>
               <Card.Body>
-                <Card.Title align="left"><h5><em>Go the extra mile</em></h5></Card.Title>
-                <Card.Text align="left">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consequat venenatis est non euismod. Mauris quis sem ex. Sed eros nulla, gravida ut diam et, lobortis ullamcorper nibh. Mauris consequat faucibus quam, id suscipit massa mollis in. Curabitur rutrum magna dui. Vivamus vitae finibus est, id tincidunt velit. Mauris pellentesque magna vel magna fringilla, nec tincidunt sapien blandit.
-                </Card.Text>
+                <Row>
+                  <Col sm={3}>
+                    <img src='donate.svg' height='80'></img>
+                  </Col>
+                  <Col sm={9}>
+                    <Card.Title align="left"><h5><em>Go the extra mile</em></h5></Card.Title>
+                    <Card.Text align="left">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consequat venenatis est non euismod. Mauris quis sem ex. Sed eros nulla, gravida ut diam et, lobortis ullamcorper nibh. Mauris consequat faucibus quam, id suscipit massa mollis in. Curabitur rutrum magna dui. Vivamus vitae finibus est, id tincidunt velit. Mauris pellentesque magna vel magna fringilla, nec tincidunt sapien blandit.  </Card.Text>
+                  </Col>
+                </Row>
+                <br></br>
                 <Row>
                   <Col>
-                    <Button style={{ backgroundColor: '#F5F5F5', borderColor: "#F5F5F5", color: "#4C4AC2" }}>Go somewhere</Button>
+                    <Button style={{ backgroundColor: '#F5F5F5', borderColor: "#F5F5F5", color: "#4C4AC2" }} href="https://donate.polarisproject.org/page/69978/donate/1?ea.tracking.id=2020HP1" target="_blank">Polaris</Button>
                   </Col>
                   <Col>
-                    <Button style={{ backgroundColor: '#F5F5F5', borderColor: "#F5F5F5", color: "#4C4AC2" }}>Go somewhere</Button>
+                    <Button style={{ backgroundColor: '#F5F5F5', borderColor: "#F5F5F5", color: "#4C4AC2" }} href="https://www.stopthetraffik.org/donate/" target="_blank">Stop the Traffik</Button>
                   </Col>
                   <Col>
-                    <Button style={{ backgroundColor: '#F5F5F5', borderColor: "#F5F5F5", color: "#4C4AC2" }}>Go somewhere</Button>
+                    <Button style={{ backgroundColor: '#F5F5F5', borderColor: "#F5F5F5", color: "#4C4AC2" }} href="https://act.unfoundation.org/onlineactions/SmFcWa8uHUea1iMzKrSCEg2" target="_blank">United Nations</Button>
                   </Col>
                 </Row>
               </Card.Body>
@@ -86,42 +191,46 @@ const Getinvolved = () => {
         </Row>
         <br></br>
         <Card>
-          <Card.Header style={{ color: '#4C4AC2' }} as="h5" align="left"><h4>Sign Petitions</h4></Card.Header>
+          <Card.Header style={{ color: '#4C4AC2' }} as="h5" align="left"><h4>Contact your Representatives</h4></Card.Header>
           <Card.Body>
-            <Card.Title align="left"><h5><em>Use your voice, speak up and act now</em></h5></Card.Title>
-            <Card.Text align="left">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consequat venenatis est non euismod. Mauris quis sem ex. Sed eros nulla, gravida ut diam et, lobortis ullamcorper nibh. Mauris consequat faucibus quam, id suscipit massa mollis in. Curabitur rutrum magna dui. Vivamus vitae finibus est, id tincidunt velit. Mauris pellentesque magna vel magna fringilla, nec tincidunt sapien blandit. Curabitur imperdiet, lacus at varius elementum, leo lorem interdum dui, tempus bibendum sem tellus eget ante.
-          </Card.Text>
-            <Accordion >
-              <Card>
-                <Accordion.Toggle as={Card.Header} eventKey="0">
-                  Human Trafficking
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>Hello! I'm the body</Card.Body>
-                </Accordion.Collapse>
-              </Card>
-              <Card>
-                <Accordion.Toggle as={Card.Header} eventKey="1">
-                  Sex Trafficking
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey="1">
-                  <Card.Body>Hello! I'm another body</Card.Body>
-                </Accordion.Collapse>
-              </Card>
-              <Card>
-                <Accordion.Toggle as={Card.Header} eventKey="1">
-                  Forced Sterilization
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey="1">
-                  <Card.Body>Hello! I'm another body</Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>
+            <Row>
+              <Col sm={2}>
+                <img src='Email.svg' height='100'></img>
+              </Col>
+              <Col sm={10}>
+                <Card.Title align="left"><h5><em>Use your voice, speak up and act now</em></h5></Card.Title>
+                <Card.Text align="left">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consequat venenatis est non euismod. Mauris quis sem ex. Sed eros nulla, gravida ut diam et, lobortis ullamcorper nibh. Mauris consequat faucibus quam, id suscipit massa mollis in. Curabitur rutrum magna dui. Vivamus vitae finibus est, id tincidunt velit. Mauris pellentesque magna vel magna fringilla, nec tincidunt sapien blandit. Curabitur imperdiet, lacus at varius elementum, leo lorem interdum dui, tempus bibendum sem tellus eget ante.
+              </Card.Text>
+                <Row>
+                  <Col>
+                    <Button style={{ backgroundColor: '#F5F5F5', borderColor: "#F5F5F5", color: "#4C4AC2", size: "lg" }} block onClick={() => HTsetModalShow(true)}>Human Trafficking</Button>
+                    <HTEmail
+                      show={HTmodalShow}
+                      onHide={() => HTsetModalShow(false)}
+                    />
+                  </Col>
+                  <Col>
+                    <Button style={{ backgroundColor: '#F5F5F5', borderColor: "#F5F5F5", color: "#4C4AC2", size: "lg" }} block onClick={() => STsetModalShow(true)}>Sex Trafficking</Button>
+                    <STEmail
+                      show={STmodalShow}
+                      onHide={() => STsetModalShow(false)}
+                    />
+                  </Col>
+                  <Col>
+                    <Button style={{ backgroundColor: '#F5F5F5', borderColor: "#F5F5F5", color: "#4C4AC2", size: "lg" }} block onClick={() => FSsetModalShow(true)}>Forced Sterilization</Button>
+                    <FSEmail
+                      show={FSmodalShow}
+                      onHide={() => FSsetModalShow(false)}
+                    />
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
           </Card.Body>
         </Card>
         <br></br>
-      </Container>
+      </Container >
     </main >
   );
 };
---
+++ src/components/TemplatedEmail.js
@@ -0,0 +1,144 @@
+import React, { useRef, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import { Form, Field } from 'react-final-form'
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
+import { TextareaAutosize } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    title: {
+        color: "#52616B"
+    },
+    textArea: {
+        margin: '5% auto',
+        width: '90%'
+    },
+    copyButton: {
+        margin: '2.5%'
+    }
+}));
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+
+const onSubmit = async values => {
+    await sleep(300)
+    window.alert(JSON.stringify(values, 0, 2))
+}
+
+const required = value => (value ? undefined : 'Required')
+
+const TemplatedEmail = () => {
+
+    const classes = useStyles();
+
+    const [copySuccess, setCopySuccess] = useState('');
+    const textAreaRef = useRef(null);
+
+    const recipients = [
+        {
+            value: 'rep1',
+            label: 'Representative 1'
+        },
+        {
+            value: 'rep2',
+            label: 'Representative 2'
+        }
+    ]
+
+    function copyToClipboard(e) {
+        textAreaRef.current.select();
+        document.execCommand('copy');
+        e.target.focus();
+        setCopySuccess('Copied!');
+        alert('Copied email template to clipboard!');
+    };
+
+    function getToday() {
+        var today = new Date();
+        var date = (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear();
+        return (
+            date + "\n"
+        );
+    }
+
+    function getEmailAttention() {
+        let specialPlaceholder = "(FIRST NAME) (LAST NAME)"
+        return (
+            "\nThe Honorable " + specialPlaceholder + "\n" + "(CAPITOL ADDRESS)" + "\n"
+        )
+    }
+
+    function getSubject() {
+        return (
+            "RE: (state the topic or include the bill number, author and subject if you are writing to support or oppose a particular legislative bill) \n"
+        )
+    }
+
+    function getEmailContent() {
+        return (
+            "Dear (ASSEMBLY MEMBER/SENATOR) (LAST NAME) My name is (YOUR FIRST NAME, LAST NAME) and I am a regional center consumer (family member/service provider/advocate/community member) who resides in your district. \n \n(State why you support or oppose the bill or other issue here. Choose up to three of the strongest points that support your position and state them clearly.) \n \n(Include a personal story. Tell your representative why the issue is important to you and how it affects you, your family member and your community.) (Tell your representative how you want her or him to vote on this issue and ask for a response. Be sure to include your name and address on both your letter and envelope.)"
+        )
+    }
+
+    function getClosing() {
+        return (
+            "\n \nSincerely, \n \n" + "(YOUR FIRST AND LAST NAME) \n" + "(STREET ADDRESS) \n(CITY), (STATE), (ZIP CODE)"
+        )
+
+    }
+
+    return (
+        <Form
+            onSubmit={onSubmit}
+            render={({ handleSubmit, reset, submitting, pristine, values }) => (
+                <form onSubmit={handleSubmit}>
+                    <Card variant="outlined">
+                        <CardContent>
+                            <Typography
+                                className={classes.title}
+                                variant="h6"
+                            >
+                                Templated Email to Representative
+                    </Typography>
+                        </CardContent>
+                        <Divider light />
+                        <Grid
+                            container
+                            spacing={2}
+                        >
+                            <Grid item xs={12}>
+                                <TextareaAutosize
+                                    className={classes.textArea}
+                                    ref={textAreaRef}
+                                    rowsMin={25}
+                                    placeholder='Type or edit your email here'
+                                    defaultValue={
+                                        getToday() +
+                                        getEmailAttention() +
+                                        getEmailContent() +
+                                        getClosing()
+                                    }
+                                />
+                            </Grid>
+                        </Grid>
+                    </Card>
+                    <Button
+                        className={classes.copyButton}
+                        style={{ backgroundColor: '#4C4AC2', borderColor: "#F5F5F5", color: "#F5F5F5" }}
+                        onClick={copyToClipboard}
+                    >
+                        Copy
+            </Button>
+                </form>
+
+            )}
+        />
+    );
+};
+
+export default TemplatedEmail;
