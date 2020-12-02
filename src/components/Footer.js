573f57ad1f13a031c11995f43e68cde006cf5571
Haoyang Chen <haoyangchen0901@gmail.com>
2020-12-02 11:33:43 -0800

    Update Footer Image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
           <div class="col-lg-2 col-md-12 mb-4">
             <div class="view overlay z-depth-1-half">
               <img
-                src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(73).jpg"
+                src="https://blog.theexodusroad.com/hs-fs/hubfs/clem-onojeghuo-AZl7zXGSSAE-unsplash.jpg?width=967&name=clem-onojeghuo-AZl7zXGSSAE-unsplash.jpg"
                 class="img-fluid"
                 alt=""
               />
@@ -22,7 +22,7 @@ const Footer = () => {
           <div class="col-lg-2 col-md-6 mb-4">
             <div class="view overlay z-depth-1-half">
               <img
-                src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(78).jpg"
+                src="https://www.lipstickandpolitics.com/wp-content/uploads/2019/02/eye-for-ebony-415494-unsplash.jpg"
                 class="img-fluid"
                 alt=""
               />
@@ -35,7 +35,7 @@ const Footer = () => {
           <div class="col-lg-2 col-md-6 mb-4">
             <div class="view overlay z-depth-1-half">
               <img
-                src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(79).jpg"
+                src="https://blog.theexodusroad.com/hs-fs/hubfs/zeyn-afuang-9xp0AWvlGC4-unsplash.jpg?width=818&name=zeyn-afuang-9xp0AWvlGC4-unsplash.jpg"
                 class="img-fluid"
                 alt=""
               />
@@ -48,7 +48,7 @@ const Footer = () => {
           <div class="col-lg-2 col-md-12 mb-4">
             <div class="view overlay z-depth-1-half">
               <img
-                src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(81).jpg"
+                src="https://www.freedomnowinternational.org/wp-content/uploads/5e1e279a110e358214ddea6b_nadine-shaabana-DRzYMtae-vA-unsplash-copy-2-p-1080.jpeg"
                 class="img-fluid"
                 alt=""
               />
@@ -61,7 +61,7 @@ const Footer = () => {
           <div class="col-lg-2 col-md-6 mb-4">
             <div class="view overlay z-depth-1-half">
               <img
-                src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(82).jpg"
+                src="https://images.squarespace-cdn.com/content/v1/5bbb96e751f4d46e7ae122e1/1558074089881-IJF92LB0X3XWU4UB53SK/ke17ZwdGBToddI8pDm48kLkXF2pIyv_F2eUT9F60jBl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iyqMbMesKd95J-X4EagrgU9L3Sa3U8cogeb0tjXbfawd0urKshkc5MgdBeJmALQKw/niu-niu-600592-unsplash.jpg"
                 class="img-fluid"
                 alt=""
               />
@@ -74,7 +74,7 @@ const Footer = () => {
           <div class="col-lg-2 col-md-6 mb-4">
             <div class="view overlay z-depth-1-half">
               <img
-                src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(84).jpg"
+                src="https://s26643.pcdn.co/wp-content/uploads/2019/01/aliyah-jamous-1058056-unsplash-e1546546945980.jpg"
                 class="img-fluid"
                 alt=""
               />
